e0562f7d17c14fcac9fb1599b5a84f1ba1bd6610
fishandchipsing <simonerebaudengo@gmail.com>
2015-05-12 14:52:46 +0200

    Added Servo examples

---
+++ day2/ServoMovements/Set_Behaviours.ino
@@ -0,0 +1,41 @@
+void setBehaviour1(){
+  servoStop(30);
+}
+
+void setBehaviour2(){
+  servoOnlyRight();
+  
+}
+
+void setBehaviour3(){
+  servoOnlyLeft();
+}
+
+void setBehaviour4(){
+  servoChill();
+}
+void setBehaviour5(){
+  servoAnxious();
+}
+
+void setBehaviour6(){
+  servoDemocratic();
+}
+
+void setBehaviour7(){
+  servoFocusLeft();
+  
+}
+
+void setBehaviour8(){
+  servoFocusRight();
+  servoStop(500);
+}
+
+void setBehaviour9(){ 
+  bouncyServo();
+}
+
+
+
+
